a74ef61895b6abb0c75a4c60425c0d0be12531aa
Marcos V Silva <mrcsvs@outlook.com>
2025-08-29 19:01:09 -0300

    Home search area

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,39 @@
-import { StyleSheet, Text, View } from "react-native";
-import Header from "../../components/Header";
+import { StyleSheet, View } from "react-native";
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+import { useState } from "react";
 
 export default function Home() {
+  const [searchInput, setSeatchInput] = useState("");
+
   return (
-    <View style={styles.container}>
+    <>
       <Header />
-      <Text>Tela de Home</Text>
-    </View>
+      <View style={styles.container}>
+        <View style={styles.inputArea}>
+          <Input
+            placeholder="Procurando algum carro?"
+            value={searchInput}
+            onChangeText={setSeatchInput}
+          />
+        </View>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    alignItems: "center",
+    backgroundColor: "#f3f5f8",
+    flex: 1,
+    paddingHorizontal: 14,
+  },
+  inputArea: {
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    marginTop: 14,
+    padding: 8,
+    width: "100%",
+  },
 });
