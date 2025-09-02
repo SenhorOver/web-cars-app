6a5a9b791dd266f0461e877a24158a7955ddb613
Marcos V Silva <mrcsvs@outlook.com>
2025-09-02 00:54:39 -0300

    Add Label Component - Details page

---
+++ src/pages/Detail/components/Label.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, View } from "react-native";
+
+interface LabelProps {
+  label: string;
+  name?: string;
+}
+
+export function Label({ name, label }: LabelProps) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.name}>{label}</Text>
+      <Text style={styles.title}>{name}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  name: {
+    color: "#444",
+  },
+  title: {
+    color: "#000",
+    fontWeight: "500",
+  },
+});
